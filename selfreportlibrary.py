9b6e40ec2bfd11ec6c3450bb761eaa8688638455
Dan Fitch <dgfitch@gmail.com>
2019-03-19 13:09:31 -0500

    Add missing constructs listing to encourage adding metadata!

--- selfreportlibrary.py
+++ selfreportlibrary.py
@@ -6,6 +6,8 @@ import re
 def run(mother):
     category = mother.categories['Self Report Measure']
     constructs = {}
+    has_constructs = []
+    missing_constructs = []
     for page in category:
         logging.debug("Checking self report", page.name)
         text = page.text()
@@ -18,21 +20,36 @@ def run(mother):
                 for c in words:
                     constructs[c] = constructs.get(c, [])
                     constructs[c].append(page.name)
+                    has_constructs.append(page.name)
+
+        if not page.name in has_constructs:
+            missing_constructs.append(page.name)
 
     logging.debug("Got constructs: ", constructs)
 
     # Now constructs contains a hash of construct -> list of pages.
+    # NOTE: This re-uses MediaWiki's category CSS classes to get three-column display. Maybe weird to do that?
     oldtext = category.text()
     cat = mwparserfromhell.parse(oldtext)
     sections = cat.get_sections()
     title = "== Sorted by Construct =="
     newtext = title + "\n\nPlease note: this section is created automatically based on the constructs in each measure's infobox. To edit, go to the measure's page and choose 'Edit with form'.\n\n"
+    newtext += "<div class='mw-category'>"
 
     # Build up an index by construct
     for k in constructs:
-        newtext += "===" + k + "===\n"
+        newtext += "<div class='mw-category-group'><h3>" + k + "</h3>\n"
         for measure in constructs[k]:
             newtext += "* [[" + measure + "]]\n"
+        newtext += "</div>"
+
+    # List out things that are missing constructs
+    newtext += "<div class='mw-category-group'><h3>No constructs listed</h3>\n"
+    for m in missing_constructs:
+        newtext += "* [[" + m + "]]\n"
+    newtext += "</div>"
+
+    newtext += "</div>\n\n"
 
     # Replace the "Sorted by Construct" section with our new text
     old_section = cat.get_sections(matches = "Sorted by Construct")[0]
